d7a04876ba717b35febd8303feba455bc8b1835e
Mateus Lima <mateuslimadev@gmail.com>
2021-07-10 20:15:17 -0300

    Exercicio incrementar porcentagem em salario de funcionario

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,6 @@
 package application;
 
-import entities.Rectangle;
+import entities.Employee;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -12,14 +12,22 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Rectangle rectangle = new Rectangle();
+        Employee employee = new Employee();
 
-        System.out.println("Enter Rectangle width");
-        rectangle.width = sc.nextDouble();
-        System.out.println("Enter Rectangle height");
-        rectangle.height = sc.nextDouble();
+        System.out.println("Nome: ");
+        employee.name = sc.nextLine();
+        System.out.println("Gross salary: ");
+        employee.grossSalary = sc.nextDouble();
+        System.out.println("Tax: ");
+        employee.tax = sc.nextDouble();
 
-        System.out.print(rectangle);
+        System.out.println(employee);
+
+        System.out.println("Wich percentage to increase salary?");
+        double percentage = sc.nextDouble();
+        employee.increaseSalary(percentage);
+        System.out.println();
+        System.out.printf("Update data: " + employee);
 
         sc.close();
     }
