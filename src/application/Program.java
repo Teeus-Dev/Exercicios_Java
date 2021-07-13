09861e1eb5dccc4e21c6b25c428eab699fb947f4
Mateus Lima <mateuslimadev@gmail.com>
2021-07-13 06:51:35 -0300

    Converter Dollar para Real com imposto IOF (Membros Estáticos)

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
-import entities.Calculator;
 
+import entities.CurrencyConverter;
+
+import javax.swing.plaf.ColorUIResource;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -12,15 +14,14 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("Enter radius: ");
-        double radius = sc.nextDouble();
+        System.out.print("What is the dollar price? ");
+        double dollarPrice = sc.nextDouble();
+        System.out.print("How many dollars will be bought? ");
+        double amount = sc.nextDouble();
+        double result = CurrencyConverter.dollarToReal(amount, dollarPrice);
+        System.out.printf("Amount to be paid in reais = %.2f%n", result);
 
-        double c = Calculator.circumference(radius);
-
-        double v = Calculator.volume(radius);
-
-        System.out.printf("Circumference: %.2f%n", c);
-        System.out.printf("Volume: %.2f%n", v);
-        System.out.printf("PI value: %.2f%n", Calculator.PI);
+        sc.close();
     }
+
 }
