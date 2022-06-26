b6f2725f66bfd3597ef18a14abb6a1f761e81916
Mateus Lima <mateuslimadev@gmail.com>
2022-06-26 20:39:19 -0300

    Programa que calcula fatorial de número inteiro

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,22 @@
 import java.util.Scanner;
 
 /*
-Desenvolva um gerador de tabuada,
-capaz de gerar a tabuada de qualquer número inteiro entre 1 a 10.
-O usuário deve informar de qual número ele deseja ver a tabuada.
-A saída deve ser conforme o exemplo abaixo:
-
-Tabuada de 5:
-5 x 1 = 5
-5 x 2 = 10
-...
-5 x 10 = 50
+Faça um programa que calcule o fatorial de um número inteiro
+fornecido pelo usuário.
+Ex.: 5!= 120 (5 x 4 x 3 x 2 x 1)
 */
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
 
-        System.out.println("Tabuada de de qual núemro? ");
-        int tabuada = sc.nextInt();
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+        int multiplacacao = 1;
 
-        for (int i = 0; i <= 10 ; i++){
-        System.out.println("Tabuada " + tabuada + " X " + i + " = "
-                + tabuada * i);
+        for (int i = fatorial ; i > 0 ; i-- ){
+            multiplacacao *= i;
         }
+        System.out.println(fatorial + "! = " + multiplacacao);
     }
 }
