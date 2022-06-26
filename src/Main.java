846d01e364a71e56651e91249cb28bfe7413fb69
Mateus Lima <mateuslimadev@gmail.com>
2022-06-26 20:20:29 -0300

    Ler "N" números e informar quantidade de Ímpares e Pares

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,34 @@
 import java.util.Scanner;
 
 /*
-Faça um programa que leia 5 números e informe o maior
-número e a média desses números.
+Faça um programa que peça N números inteiros,
+calcule e mostre a quantidade de números pares
+e a quantidade de números impares.
 */
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int quantidadeNumeros;
         int numero;
-        int soma = 0;
-        int maior = 0;
-        int count = 0;
+        int quantidadePares = 0, quantidadeImpares = 0;
 
+        System.out.println("Quantos números, vc quer calcular?");
+        quantidadeNumeros = sc.nextInt();
+
+        int count = 0;
         do {
             System.out.println("Digite o número");
             numero = sc.nextInt();
-            soma += numero;
-            if (numero > maior)
-                    maior = numero;
-            count ++;
-        } while (count < 5);
 
-        System.out.println("Maior " + maior);
-        System.out.println("Media " + soma/5);
+            if (numero % 2 == 0) quantidadePares++;
+            else quantidadeImpares++;
+
+            count ++;
+        } while (count < quantidadeNumeros);
+
+        System.out.println("Quantidade de Pares " + quantidadePares);
+        System.out.println("Quantidade Impares " + quantidadeImpares);
     }
 }
