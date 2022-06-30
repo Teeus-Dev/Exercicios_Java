a723944501c5cb632e66251a3ae3e47145d958cc
Mateus Lima <mateuslimadev@gmail.com>
2022-06-29 22:00:06 -0300

    Array Multidimensional

--- src/Main.java
+++ src/Main.java
@@ -11,9 +11,9 @@ public class Main {
 
         int[][] M = new int[4][4];
 
-        for(int i = 0; i < M.length; i++) {
-            for(int j = 0; j < M[i].length; j++) {
-                M[i][j] = random.nextInt(9);
+        for(int linha = 0; linha < M.length; linha++) {
+            for(int coluna = 0; coluna < M[linha].length; coluna++) {
+                M[linha][coluna] = random.nextInt(9);
             }
         }
 
